234fe86326f3a34492a6072161bb2e16250e9b79
shehzad-haroon <56392887+shehzad-haroon@users.noreply.github.com>
2021-02-18 16:10:15 +0500

    new change in app file

--- expense-tracker-app/src/App.js
+++ expense-tracker-app/src/App.js
@@ -4,18 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>helo </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello Everyone </h1>
     </div>
   );
 }
